9623b27a6a03643b3f8e2054b0ea394d0e388bae
Anatolii Tupkalo <tigerforce@mail.ru>
2023-02-14 20:03:00 -0600

    The ctructure of the app has been setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import './header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                <h1 className="title"> Go Places bucket list</h1>
+            </header>
+        );
+    }
+}
+
+export default Header;
